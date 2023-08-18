6d4a8eeef7edd38e88a8b9392c3985ce43815bc4
jodan <wdd789q@naver.com>
2023-08-18 23:14:14 +0900

    팝업창 개발 완료

--- hackerton/src/UI/Card.jsx
+++ hackerton/src/UI/Card.jsx
@@ -1,8 +1,11 @@
 
 import styled, { css } from 'styled-components';
-import { Link ,Route, Routes } from "react-router-dom"; // Import Link component
+import { Link, Route, Routes } from "react-router-dom"; // Import Link component
 import React, { useState } from 'react';
-
+import Notion from '../asset/Notion.png';
+import github from '../asset/github.png';
+import sparkles from '../asset/sparkles.png';
+import Green from '../asset/Green.png';
 
 const Header = styled.div`
   display: flex;
@@ -106,7 +109,7 @@ const SecondName = styled.div`
 const SecondText = styled.div`
   margin-left: 1rem;
   margin-top: 2rem;
-  colo  r: var(--grey-thickest, #626468);
+  color: var(--grey-thickest, #626468);
   font-family: Inter;
   font-size: 13px;
   font-style: normal;
@@ -150,21 +153,161 @@ const ModalOverlay = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  
 `;
 
 const ModalContent = styled.div`
   background-color: white;
   border-radius: 10px;
-  padding: 20px;
+  padding: 30px; /* Increase the padding for more space */
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2);
-  max-width: 80%;
+  width: 700px;
+  height: 400px;
+  overflow: hidden; /* Hide the scrollbars */
 `;
+
+
+const SmallPictureBox = styled.div`
+  border-radius: 20px;
+  border: 1px solid #EBEBEB;
+  width: 300px;
+  height: 177px;
+  flex-shrink: 0;
+  margin-left: 1rem;
+`;
+
+const SmallNameBox = styled.div`
+  margin-left: 8rem;
+  margin-top: 2rem;
+  color: var(--black-point, #27292C);
+  font-family: Inter;
+  font-size: 27px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+const SmallTextBox = styled.div`
+  margin-left: 1rem;
+  margin-top: 2rem;
+  color: var(--black-point, #27292C);
+  font-family: Inter;
+  font-size: 17px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+const SmallIntroduceBox = styled.div`
+  margin-left: 5rem;
+  margin-top: 1rem;
+  color: var(--blue-point, #0068FF);
+  font-family: Inter;
+  font-size: 17px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+const SmallButtonBox = styled.button`
+  margin-left: 2rem;
+  margin-top: 1rem;
+  border-radius: 10px;
+  background: var(--blue-point, #0068FF);
+  color: White;
+  width: 269px;
+  height: 51px;
+  flex-shrink: 0;
+`;
+
+
+const PopupBox = styled.div`
+  display: flex;
+`;
+
+const LeftBox = styled.div`
+  
+`;
+
+const RightBox = styled.div`
+  
+`;
+
+const Delete = styled.button`
+  border: none;
+  margin-left: 20rem;
+  background-color:transparent;
+  font-family: Inter;
+  font-size: 25px;
+  font-style: normal;
+  font-weight: 500;
+  
+
+  color:grey;
+  position: absolute;
+`;
+
+
+
+const IntroduceBox = styled.div`
+
+  color: var(--black-point, #27292C);
+  font-family: Inter;
+  font-size: 27px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  margin-top: 1rem;
+`;
+
+const NotionBox = styled.div`
+
+`;
+
+const VerySmallBox = styled.div`
+  margin-top: 1rem;
+  margin-left: 1rem;
+  border-radius: 15px;
+  border: 1px solid var(--grey-light, #F4F6F9);
+  background: #FFF;
+  width: 33px;
+  height: 33px;
+  flex-shrink: 0;
+
+  display: flex;
+`;
+
+const NotionTitle = styled.div`
+
+  margin-left: 1rem;
+  color: var(--black-point, #27292C);
+  font-family: Inter;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+const NotionLink = styled.div`
+
+  margin-left: 1rem;
+  color: var(--blue-point, #0068FF);
+  font-family: Inter;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  width: 600px;
+`;
+
+
+
 
 
 export default function Card() {
 
   const [selectedProfile, setSelectedProfile] = useState(null);
-  
+
   const profiles = [
     {
       name: '김성섭',
@@ -175,112 +318,112 @@ export default function Card() {
     },
   ];
 
-    // Modal open function
-    const openModal = (profile) => {
-      setSelectedProfile(profile);
-    };
-  
-    // Modal close function
-    const closeModal = () => {
-      setSelectedProfile(null);
-    };
+  // Modal open function
+  const openModal = (profile) => {
+    setSelectedProfile(profile);
+  };
 
-    return (
-      <>
-        <Header>
-          <Logo>
-           여기카드페이지
-          </Logo>
-          <Home>
-            <Link to="/" onfocus="this.blur()" >홈</Link>
-          </Home>
-          <Register>
-            <Link to="/Card" onfocus="this.blur()" >명함</Link>
-          </Register>
-          <Login>
-            <Link to="/login" onfocus="this.blur()" >로그인</Link>
-          </Login>
-        </Header>
-        <Border/>
+  // Modal close function
+  const closeModal = () => {
+    setSelectedProfile(null);
+  };
+
+  return (
+    <>
+      <Header>
+        <Logo>
+          여기카드페이지
+        </Logo>
+        <Home>
+          <Link to="/"  >홈</Link>
+        </Home>
+        <Register>
+          <Link to="/Card"  >명함</Link>
+        </Register>
+        <Login>
+          <Link to="/login"  >로그인</Link>
+        </Login>
+      </Header>
+      <Border />
       <SecondBox>
         <SecondBody>
-        {profiles.map((profile, index) => (
-          <PictureBox key={index} onClick={() => openModal(profile)}>
-                      <PictureBox>
-            <Picture>
-              사진자리
-            </Picture>
-            <SecondName>
-              김성섭
-            </SecondName>
-            <SecondText>
-              새로운 것을 즐기는 개발자 김성섭입니다.
-            </SecondText>
-            <SecondIntroduce>
-              22학번 / 모바일개발자
-            </SecondIntroduce>
-            <Address>
-              https://github.com/wakathon-dont-worry-team-name/
-            </Address>
-            <Address>
-              https://notion.com/wakathon-dont-worry-team-name/
-            </Address>
-          </PictureBox>
-          </PictureBox>
+          {profiles.map((profile, index) => (
+            <PictureBox key={index} onClick={() => openModal(profile)}>
+              <PictureBox>
+                <Picture>
+                  사진자리
+                </Picture>
+                <SecondName>
+                  김성섭
+                </SecondName>
+                <SecondText>
+                  새로운 것을 즐기는 개발자 김성섭입니다.
+                </SecondText>
+                <SecondIntroduce>
+                  22학번 / 모바일개발자
+                </SecondIntroduce>
+                <Address>
+                  https://github.com/wakathon-dont-worry-team-name/
+                </Address>
+                <Address>
+                  https://notion.com/wakathon-dont-worry-team-name/
+                </Address>
+              </PictureBox>
+            </PictureBox>
           ))}
         </SecondBody>
 
         <SecondBody>
-        {profiles.map((profile, index) => (
-          <PictureBox key={index} onClick={() => openModal(profile)}>
-                      <PictureBox>
-            <Picture>
-              사진자리
-            </Picture>
-            <SecondName>
-              김성섭
-            </SecondName>
-            <SecondText>
-              새로운 것을 즐기는 개발자 김성섭입니다.
-            </SecondText>
-            <SecondIntroduce>
-              22학번 / 모바일개발자
-            </SecondIntroduce>
-            <Address>
-              https://github.com/wakathon-dont-worry-team-name/
-            </Address>
-            <Address>
-              https://notion.com/wakathon-dont-worry-team-name/
-            </Address>
-          </PictureBox>
-          </PictureBox>
+          {profiles.map((profile, index) => (
+            <PictureBox key={index} onClick={() => openModal(profile)}>
+              <PictureBox>
+                <Picture>
+                  사진자리
+                </Picture>
+                <SecondName>
+                  김성섭
+                </SecondName>
+                <SecondText>
+                  새로운 것을 즐기는 개발자 김성섭입니다.
+                </SecondText>
+                <SecondIntroduce>
+                  22학번 / 모바일개발자
+                </SecondIntroduce>
+                <Address>
+                  https://github.com/wakathon-dont-worry-team-name/
+                </Address>
+                <Address>
+                  https://notion.com/wakathon-dont-worry-team-name/
+                </Address>
+              </PictureBox>
+            </PictureBox>
           ))}
         </SecondBody>
 
         <SecondBody>
-        {profiles.map((profile, index) => (
-          <PictureBox key={index} onClick={() => openModal(profile)}>
-                      <PictureBox>
-            <Picture>
-              사진자리
-            </Picture>
-            <SecondName>
-              김성섭
-            </SecondName>
-            <SecondText>
-              새로운 것을 즐기는 개발자 김성섭입니다.
-            </SecondText>
-            <SecondIntroduce>
-              22학번 / 모바일개발자
-            </SecondIntroduce>
-            <Address>
-              https://github.com/wakathon-dont-worry-team-name/
-            </Address>
-            <Address>
-              https://notion.com/wakathon-dont-worry-team-name/
-            </Address>
-          </PictureBox>
-          </PictureBox>
+          {profiles.map((profile, index) => (
+            <PictureBox key={index} onClick={() => openModal(profile)}>
+              <PictureBox>
+                <Picture>
+                  사진자리
+                </Picture>
+                <SecondName>
+                  김성섭
+                </SecondName>
+                <SecondText>
+                  새로운 것을 즐기는 개발자 김성섭입니다.
+                </SecondText>
+                <SecondIntroduce>
+                  22학번 / 모바일개발자
+                </SecondIntroduce>
+                <Address>
+                  https://github.com/wakathon-dont-worry-team-name/
+                </Address>
+                <Address>
+                  https://notion.com/wakathon-dont-worry-team-name/
+                </Address>
+              </PictureBox>
+            </PictureBox>
           ))}
         </SecondBody>
       </SecondBox>
@@ -289,13 +432,92 @@ export default function Card() {
       {selectedProfile && (
         <ModalOverlay>
           <ModalContent>
-            {/* Render profile details */}
-            <h2>{selectedProfile.name}</h2>
-            <p>{selectedProfile.introduction}</p>
-            <p>{selectedProfile.school}</p>
-            {/* ... Add more profile details ... */}
-            <button onClick={closeModal}>Close</button>
+            <PopupBox>
+              <LeftBox>
+                <SmallPictureBox>사진자리</SmallPictureBox>
+                <SmallNameBox>{selectedProfile.name}</SmallNameBox>
+                <SmallIntroduceBox>{selectedProfile.school}</SmallIntroduceBox>
+                <SmallTextBox>{selectedProfile.introduction}</SmallTextBox>
+                <SmallButtonBox>카카오톡 연락하기</SmallButtonBox>
+
+              </LeftBox>
+              <RightBox>
+                <Delete onClick={closeModal}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M4.96209 4.96209C5.3282 4.59597 5.9218 4.59597 6.28791 4.96209L15 13.6742L23.7121 4.96209C24.0782 4.59597 24.6718 4.59597 25.0379 4.96209C25.404 5.3282 25.404 5.9218 25.0379 6.28791L16.3258 15L25.0379 23.7121C25.404 24.0782 25.404 24.6718 25.0379 25.0379C24.6718 25.404 24.0782 25.404 23.7121 25.0379L15 16.3258L6.28791 25.0379C5.9218 25.404 5.3282 25.404 4.96209 25.0379C4.59597 24.6718 4.59597 24.0782 4.96209 23.7121L13.6742 15L4.96209 6.28791C4.59597 5.9218 4.59597 5.3282 4.96209 4.96209Z" fill="#A4A5A8" stroke="#A4A5A8" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+                </Delete>
+                <IntroduceBox>
+                  소개 링크
+                  <VerySmallBox>
+                    <img src={Notion} />
+                    <NotionBox>
+                      <NotionTitle>
+                        내 노션 
+                      </NotionTitle>
+                      <NotionLink>
+                        https://www.notion.so/jodandan
+                      </NotionLink>
+                    </NotionBox>
+                  </VerySmallBox>
+
+                  <VerySmallBox>
+                    <img src={github} />
+                    <NotionBox>
+                      <NotionTitle>
+                        내 깃허브 
+                      </NotionTitle>
+                      <NotionLink>
+                        https://github.com/jodandan
+                      </NotionLink>
+                    </NotionBox>
+                  </VerySmallBox>
+                </IntroduceBox>
+
+                <IntroduceBox>
+                  프로젝트
+                  <VerySmallBox>
+                    <img src={sparkles} />
+                    <NotionBox>
+                      <NotionTitle>
+                        심판다 
+                      </NotionTitle>
+                      <NotionLink>
+                        모바일 개발자
+                      </NotionLink>
+                    </NotionBox>
+                  </VerySmallBox>
+
+                  <VerySmallBox>
+                    <img src={Green} />
+                    <NotionBox>
+                      <NotionTitle>
+                        모두의 정원 
+                      </NotionTitle>
+                      <NotionLink>
+                        모바일 개발자
+                      </NotionLink>
+                    </NotionBox>
+                  </VerySmallBox>
+
+                  <VerySmallBox>
+                    <img src={sparkles} />
+                    <NotionBox>
+                      <NotionTitle>
+                        판다 
+                      </NotionTitle>
+                      <NotionLink>
+                        모바일 개발자
+                      </NotionLink>
+                    </NotionBox>
+                  </VerySmallBox>
+                  
+                </IntroduceBox>
+
+              </RightBox>
+            </PopupBox>
           </ModalContent>
+
         </ModalOverlay>
       )}
     </>
